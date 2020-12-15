29a222c5b597d1fc0f59832ecf8cd7480fe0b5c0
smccowan3 <68258345+smccowan3@users.noreply.github.com>
2020-12-15 16:10:15 +1100

    rock and sceneupdates

---
+++ Assets/Scripts/PlayerAttributes.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    public int playerLevel = 1;
+    public bool alive = true;
+
+    [SerializeField]
+    public float jumpHeight = 5f;
+    public float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    public int breakStrength = 100;
+    bool onGround = false;
+    float dirX;
+    bool dblJump = false;
+
+    public void becomeDead()
+    {
+        alive = false;
+        playerLevel++;
+        Debug.Log("yay you died");
+    }
+
+    void breakSomething(int something)
+    {
+        if (breakStrength >= something)
+        {
+            Debug.Log("wow you broke something");
+        }
+        else if (breakStrength < something)
+        {
+            Debug.Log("darn you didnt break something");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if on ground
+        if (rb.velocity.y == 0)
+            onGround = true;
+        else
+            onGround = false;
+
+        if (onGround)
+            dblJump = true;
+
+        if (onGround && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        else if (dblJump && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+            dblJump = false;
+        }
+        dirX = Input.GetAxis("Horizontal") * moveSpeed;
+        if (rb.velocity.y <= 0.01 && rb.velocity.y >= -0.01)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.velocity = Vector2.up * jumpHeight;
+    }
+}
---
+++ Assets/Scripts/rock.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rockbody;
+    public Transform rocktransform;
+    private int framecount = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rockbody = GetComponent<Rigidbody2D>();
+        rocktransform = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        Debug.Log(framecount);
+        if (isStationary())
+        {
+            framecount++;
+        }
+        else if (isStationary() == false)
+        {
+            framecount = 0;
+        }
+        if (framecount > 6000)
+        {
+            Debug.Log("condition fulfilled");
+            rockbody.velocity = Vector2.down;
+        }
+    }
+
+    bool isStationary()
+    {
+        bool stationary = false;
+        if (player.GetComponent<Rigidbody2D>().velocity.y == 0 && player.GetComponent<Rigidbody2D>().velocity.x == 0) //initially stationary
+        {
+            stationary = true;
+        }
+            return stationary;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        player.GetComponent<PlayerAttributes>().becomeDead();
+    }
+}
